a0543f7856a5a36d04ea6bee34dda46d0e014401
mahmoud <mahmoudnasserelmoghany@gmail.com>
2026-05-01 17:08:19 +0300

    second commit

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-def task_tracker():
-    tasks = ["Buy groceries", "Finish Jenkins task", "Learn Docker"]
+import time
 
+def task_tracker():
+    tasks = ["Learn Java", "Finish Jenkins task", "Use Docker"]
+    
     print("--- My Task Tracker ---")
     for index, task in enumerate(tasks, 1):
         print(f"{index}. {task}")
     print("-----------------------")
+    
+    print("deployment successful, the container is now up")
+
+    while True:
+        time.sleep(60)
     return True
 
 if __name__ == "__main__":
